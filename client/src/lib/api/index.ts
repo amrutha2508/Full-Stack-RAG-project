95ed1f0930bab4551b928722d86ba98154c270f5
amrutha2508 <amrutha.k5637@gmail.com>
2026-04-30 16:16:34 -0500

    "removed token log"

--- client/src/lib/api/index.ts
+++ client/src/lib/api/index.ts
@@ -2,10 +2,9 @@
 // API Configuration
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"
-
+console.log("API_BASE_URL:", API_BASE_URL)
 export const apiClient = {
     get: async (endpoint: string, token?:string | null) => {
-        console.log("get endpoint token: ",token)
         const headers:HeadersInit = {};
         if(token) {
             headers['Authorization'] = `Bearer ${token}`;
